f11e3c3d36a6d4a760b80b21a8abd2e870a2b3cb
AarushiGOYAL362 <84665917+AarushiGOYAL362@users.noreply.github.com>
2021-06-16 17:59:56 +0530

    Create 26.c

---
+++ 26.c
@@ -0,0 +1,12 @@
+/*to find absolute value of given number*/
+#include<stdio.h>
+int main() 
+{
+   int a;
+   printf("enter any number:");   
+   scanf("%d",&a);  
+   if(a<0)
+     a=-1*a;   
+   printf("absolute value:%d",a);
+   return 0;
+}
